ec4492259738367dc9b0f86f0456932016868c22
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-10 15:39:00 +0200

    feat: rename and create linkAppContracts

--- src/presentation/components/contracts/index.ts
+++ src/presentation/components/contracts/index.ts
@@ -1,10 +1,16 @@
 import { LatLngExpression } from 'leaflet';
+import { ReactNode } from 'react';
 
 export type Place = {
   id: string;
   name: string;
   location: LatLngExpression;
 };
-export type MapProtocols = {
+export type MapContracts = {
   places?: Array<Place>;
+};
+
+export type LinkContracts = {
+  href: string;
+  children: ReactNode;
 };
