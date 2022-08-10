3ba18ae9d21e47e29608727a4812bd45d015f4c0
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-10 13:26:31 +0200

    style: add global styles

--- src/shared/styles/globals.ts
+++ src/shared/styles/globals.ts
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #e20e8d;
+    --background:#030518;
+    --white: #eeee;
+    --container: 100rem;
+    --small:1.5rem;
+    --medium:3rem;
+    --large: 5rem;
+  }
   * {
     margin: 0px;
     padding: 0px;
@@ -13,6 +22,15 @@ export const GlobalStyles = createGlobalStyle`
   body,
   #__next{
     height: 100%;
+    background: var(--background);
+    color: var(--white);
+  }
+  p{
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+  a {
+    color: var(--highlight);
   }
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
