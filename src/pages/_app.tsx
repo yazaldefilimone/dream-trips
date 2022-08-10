7447e35f9ef2406912b87803c4522ec0f8a1d7df
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-10 14:01:24 +0200

    feat: add meta tag of seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,28 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { Fragment } from 'react';
+import { Fragment, FunctionComponent } from 'react';
 import { GlobalStyles } from '~/shared/styles/globals';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   return (
     <Fragment>
       <Head>
-        <title>Next Boilerplate</title>
+        <title>My Dream Trips</title>
         <link rel="shortcut icon" href="/img-512.png" />
         <link rel="apple-touch-icon" href="/img-512.png" />
-        <meta
-          name="description"
-          content="A simple project starter to work with Typescript, ReactJs, NextJs, Styled Components and Clean Architecture with TDD "
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+          integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+          crossOrigin=""
         />
+        <meta name="author" content="Yazalde Filimone" />
+        <meta name="description" content="Simple website with spaces I want travel in future" />
       </Head>
       <GlobalStyles />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Fragment>
   );
-}
+};
 
 export default MyApp;
