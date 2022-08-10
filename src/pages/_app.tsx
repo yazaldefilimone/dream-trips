d9b71c88105db11aa22513634d750a37bc771e06
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-10 15:19:24 +0200

    chore: change link of app from document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,7 @@ const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
         <title>My Dream Trips</title>
         <link rel="shortcut icon" href="/img-512.png" />
         <link rel="apple-touch-icon" href="/img-512.png" />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
-          integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
-          crossOrigin=""
-        />
+
         <meta name="author" content="Yazalde Filimone" />
         <meta name="description" content="Simple website with spaces I want travel in future" />
       </Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,7 +29,14 @@ export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+            integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+            crossOrigin=""
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
