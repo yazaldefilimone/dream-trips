8e1fd1bb6c33cd457a3d13fdaf54526354741d97
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-10 14:01:44 +0200

    feat: start create component of map

---
+++ src/presentation/components/Map/index.tsx
@@ -0,0 +1,22 @@
+import { LatLngExpression } from 'leaflet';
+import { FunctionComponent } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+const Map: FunctionComponent = () => {
+  const position: LatLngExpression = [51.505, -0.09];
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
