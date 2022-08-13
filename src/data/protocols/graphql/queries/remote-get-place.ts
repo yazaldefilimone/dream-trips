c37d46495cde78a77d94b34756f20a533c03b1ec
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 17:08:15 +0200

    feat: create get place remote interface

---
+++ src/data/protocols/graphql/queries/remote-get-place.ts
@@ -0,0 +1,11 @@
+import { Page } from '~/domain/entities';
+import { InternalServerError } from '~/domain/errors';
+import { Either } from '~/shared/error-handler/either';
+
+export interface IRemoteGetPlace {
+  query: ({ slug }: { slug: string }) => IRemoteGetPlace.output;
+}
+
+export namespace IRemoteGetPlace {
+  export type output = Promise<Either<InternalServerError, Page>>;
+}
