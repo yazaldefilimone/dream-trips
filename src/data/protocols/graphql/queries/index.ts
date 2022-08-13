7e34565240622967d7ef40132c962d48d45d16b5
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 11:27:21 +0200

    feat: create remote get page

--- src/data/protocols/graphql/queries/index.ts
+++ src/data/protocols/graphql/queries/index.ts
@@ -1,1 +1,2 @@
 export * from './remote-get-page';
+export * from './remote-get-pages';
--- src/data/protocols/graphql/queries/remote-get-page.ts
+++ src/data/protocols/graphql/queries/remote-get-page.ts
@@ -2,10 +2,10 @@ import { Page } from '~/domain/entities';
 import { InternalServerError } from '~/domain/errors';
 import { Either } from '~/shared/error-handler/either';
 
-export interface IRemoteGetPages {
-  query: () => IRemoteGetPages.output;
+export interface IRemoteGetPage {
+  query: ({ slug }: { slug: string }) => IRemoteGetPage.output;
 }
 
-export namespace IRemoteGetPages {
-  export type output = Promise<Either<InternalServerError, Page[]>>;
+export namespace IRemoteGetPage {
+  export type output = Promise<Either<InternalServerError, Page>>;
 }
