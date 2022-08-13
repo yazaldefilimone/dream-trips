8703d11f2e9a20af7fa4381381ab0d3d764b3723
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 11:28:02 +0200

    feat: implement remote get page

---
+++ src/infra/services/graphql/queries/remote-get-page/index.ts
@@ -0,0 +1,20 @@
+import { InternalServerError } from '~/domain/errors';
+import { Page } from '~/domain/entities';
+import { IRemoteGetPage } from '~/data/protocols/graphql/queries';
+
+import { client } from '~/infra/services/graphql/client';
+import { PageQuery } from './query';
+
+import { left, right } from '~/shared/error-handler/either';
+
+export class RemoteGetPage implements IRemoteGetPage {
+  async query({ slug }: { slug: string }): IRemoteGetPage.output {
+    try {
+      const { pages } = await client.request<{ pages: Page }>(PageQuery, { slug });
+      return right(pages);
+    } catch (error: any) {
+      console.log({ message: error.message, response: error.response.headers });
+      return left(new InternalServerError());
+    }
+  }
+}
