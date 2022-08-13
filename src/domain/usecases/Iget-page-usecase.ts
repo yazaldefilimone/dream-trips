005d8dd7c1158d3e4fe2053ad612ee8571d318a2
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 11:28:50 +0200

    feat: create interface of get page

--- src/domain/usecases/Iget-page-usecase.ts
+++ src/domain/usecases/Iget-page-usecase.ts
@@ -3,8 +3,9 @@ import { Page } from '~/domain/entities';
 import { InternalServerError } from '~/domain/errors';
 
 export interface IGetPageUseCase {
-  handle: () => IGetPageUseCase.output;
+  handle: (data: IGetPageUseCase.Input) => IGetPageUseCase.output;
 }
 export namespace IGetPageUseCase {
   export type output = Promise<Either<InternalServerError, Page>>;
+  export type Input = { slug: string };
 }
