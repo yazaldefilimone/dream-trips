2e8267e39006304609b6d4fab83a40d5d51a6086
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 19:35:17 +0200

    chore: config pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-};
+const withPWA = require('next-pwa');
+
+const isProd = process.env.NODE === 'production';
+const nextConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: isProd
+  },
+  images: {
+    domains: ['media.graphassets.com']
+  }
+});
 
 module.exports = nextConfig;
