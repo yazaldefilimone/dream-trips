6742abce3e2fe3390de1f0b65fbe184623961db3
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 12:04:33 +0200

    feat: make static page

--- src/presentation/templates/Page/index.tsx
+++ src/presentation/templates/Page/index.tsx
@@ -3,19 +3,19 @@ import { LinkApp } from '~/presentation/components/LinkApp';
 import { PageTempleteBody, PageTempleteContainer, PageTempleteHeading } from './styles';
 
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
-
-export const PageTemplete: FunctionComponent = () => {
+type PageTempleteProps = {
+  heading: string;
+  body: string;
+};
+export const PageTemplete: FunctionComponent<PageTempleteProps> = ({ heading, body }) => {
   return (
     <PageTempleteContainer>
       <LinkApp href="/">
         <CloseOutline size={32} aria-label="close" />
       </LinkApp>
-      <PageTempleteHeading>My Dream Trips</PageTempleteHeading>
+      <PageTempleteHeading>{heading}</PageTempleteHeading>
       <PageTempleteBody>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime quaerat magnam modi. Fuga excepturi voluptas
-          ab officiis, esse earum voluptate, ut aliquid nihil, velit corrupti sequi architecto alias sit quam.
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: body }} />
       </PageTempleteBody>
     </PageTempleteContainer>
   );
