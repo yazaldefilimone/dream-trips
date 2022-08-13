8417d7866fd53c57176774223d22649ef92705e7
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 12:04:19 +0200

    feat: create static page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,12 +1,16 @@
+import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import { FunctionComponent } from 'react';
-import { getPagesFactory } from '~/infra/factories';
+import { getPageFactory, getPagesFactory } from '~/infra/factories';
 import { PageTemplete } from '~/presentation/templates/Page';
-
-const Page: FunctionComponent = () => {
+type PageProps = {
+  heading: string;
+  body: string;
+};
+const Page: FunctionComponent<PageProps> = ({ heading, body }) => {
   const router = useRouter();
   if (router.isFallback) return null;
-  return <PageTemplete />;
+  return <PageTemplete heading={heading} body={body} />;
 };
 
 export default Page;
@@ -20,15 +24,21 @@ export async function getStaticPaths() {
   const paths = pages.map(({ slug }) => ({ params: { slug } }));
   return { paths, fallback: true };
 }
-// export async function getStaticProps() {
-//   const pagesOrError = await getPagesFactory().handle();
-//   if (pagesOrError.isLeft()) {
-//     console.log({ pagesOrError });
 
-//     return { props: {} };
-//   }
-//   const pages = pagesOrError.value;
-//   console.log(pages);
-//   const paths = pages.map(({ slug }) => ({ params: { slug } }));
-//   return { props: {} };
-// }
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const slug = params?.slug ? (params?.slug as string) : '';
+  const pageOrError = await getPageFactory().handle({ slug });
+  if (pageOrError.isLeft()) {
+    return { notFound: true };
+  }
+
+  const page = pageOrError.value;
+  if (!page) return { notFound: true };
+  console.log(page);
+  return {
+    props: {
+      heading: page?.heading,
+      body: page?.body?.html
+    }
+  };
+};
