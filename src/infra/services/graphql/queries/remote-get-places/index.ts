d691f14b78d255b3e6b131ecc9a12b1660344d14
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 18:44:49 +0200

    fix: filter first place

--- src/infra/services/graphql/queries/remote-get-places/index.ts
+++ src/infra/services/graphql/queries/remote-get-places/index.ts
@@ -9,7 +9,7 @@ import { placeQuery } from './query';
 export class RemoteGetPlaces implements IRemoteGetPlaces {
   async query(): IRemoteGetPlaces.output {
     try {
-      const { places } = await client.request<{ places: Place[] }>(placeQuery);
+      const { places } = await client.request<{ places: Place[] }>(placeQuery, { first: 3 });
 
       return right(places);
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
