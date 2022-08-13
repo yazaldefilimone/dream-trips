22e55d0b205f4987a2a99b749b94a3c3a3ddc272
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 15:54:57 +0200

    feat: create place entity

---
+++ src/domain/entities/place.ts
@@ -0,0 +1,21 @@
+export type Place = {
+  id: string;
+  slug: string;
+  name: string;
+  location: Location;
+  description: Description;
+  gallery: Gallery;
+};
+
+type Gallery = {
+  url: string;
+  height: number;
+  width: number;
+};
+type Location = {
+  latitude: number;
+  longitude: number;
+};
+type Description = {
+  html: string;
+};
