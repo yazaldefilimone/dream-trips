ec761aec7900120ddeb99d44c16a29a1416c5eb7
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 09:45:24 +0200

    feat: create  interface of get-pages-usecase

--- src/domain/usecases/Iget-pages-usecase.ts
+++ src/domain/usecases/Iget-pages-usecase.ts
@@ -1,9 +1,10 @@
 import { Either } from '~/shared/error-handler/either';
 import { Page } from '~/domain/entities';
+import { InternalServerError } from '~/domain/errors';
 
 export interface IGetPagesUseCase {
   handle: () => IGetPagesUseCase.output;
 }
 export namespace IGetPagesUseCase {
-  export type output = Promise<Either<Error, Page[]>>;
+  export type output = Promise<Either<InternalServerError, Page[]>>;
 }
