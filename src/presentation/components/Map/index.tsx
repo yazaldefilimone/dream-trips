3c8a372550356a4ed652a6004326b7134d4b4634
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 18:42:58 +0200

    feat: add click event in marker

--- src/presentation/components/Map/index.tsx
+++ src/presentation/components/Map/index.tsx
@@ -3,13 +3,27 @@ import { FunctionComponent } from 'react';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
 import { MapContracts, Place } from '~/presentation/components/contracts';
+import { useRouter } from 'next/router';
 
 const Map: FunctionComponent<MapContracts> = ({ places }) => {
+  const router = useRouter();
   const position: LatLngExpression = [0, 0];
+  function handlerMarker(slug: string) {
+    return () => router.push(`/place/${slug}`);
+  }
 
   function PlaceMap(place: Place) {
     const { latitude, longitude } = place.location;
-    return <Marker key={`place-${place.id}`} title={place.name} position={[latitude, longitude]}></Marker>;
+    return (
+      <Marker
+        key={`place-${place.id}`}
+        title={place.name}
+        position={[latitude, longitude]}
+        eventHandlers={{
+          click: handlerMarker(place.slug)
+        }}
+      ></Marker>
+    );
   }
 
   return (
