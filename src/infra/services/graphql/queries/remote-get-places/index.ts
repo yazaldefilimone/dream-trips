dd99ed5c50f1926ed4a27aedb34f92927daa7376
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 16:03:37 +0200

    feat: implement remote \ get places

---
+++ src/infra/services/graphql/queries/remote-get-places/index.ts
@@ -0,0 +1,19 @@
+import { IRemoteGetPlaces } from '~/data/protocols/graphql/queries';
+import { Place } from '~/domain/entities';
+import { InternalServerError } from '~/domain/errors';
+import { client } from '~/infra/services/graphql/client';
+
+import { left, right } from '~/shared/error-handler/either';
+import { placeQuery } from './query';
+
+export class RemoteGetPlaces implements IRemoteGetPlaces {
+  async query(): IRemoteGetPlaces.output {
+    try {
+      const { places } = await client.request<{ places: Place[] }>(placeQuery, { first: 3 });
+      return right(places);
+    } catch (error: any) {
+      console.log({ message: error.message, response: error.response.headers });
+      return left(new InternalServerError());
+    }
+  }
+}
