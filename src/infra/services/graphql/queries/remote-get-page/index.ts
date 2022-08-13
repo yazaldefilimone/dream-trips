9dd5173f5c923e40a46d79bb3af3f67e5e4fae36
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-13 12:04:05 +0200

    feat: return first page

--- src/infra/services/graphql/queries/remote-get-page/index.ts
+++ src/infra/services/graphql/queries/remote-get-page/index.ts
@@ -10,10 +10,10 @@ import { left, right } from '~/shared/error-handler/either';
 export class RemoteGetPage implements IRemoteGetPage {
   async query({ slug }: { slug: string }): IRemoteGetPage.output {
     try {
-      const { pages } = await client.request<{ pages: Page }>(PageQuery, { slug });
-      return right(pages);
+      const { pages } = await client.request<{ pages: Page[] }>(PageQuery, { slug });
+      return right(pages[0]);
     } catch (error: any) {
-      console.log({ message: error.message, response: error.response.headers });
+      console.log({ message: error.message, response: error.response });
       return left(new InternalServerError());
     }
   }
